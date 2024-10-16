8edda8a89d25be9712038326b9506ba81081e991
Satya Prakash Sahoo <sahoosatyaprakash88@gmail.com>
2024-10-16 15:18:23 +0530

    add

---
+++ src/main/java/com/satya/ecom_proj/service/ProductService.java
@@ -0,0 +1,51 @@
+package com.satya.ecom_proj.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.satya.ecom_proj.model.Product;
+import com.satya.ecom_proj.repository.ProductRepository;
+
+@Service
+public class ProductService 
+{
+	@Autowired
+	private ProductRepository repository;
+	public List<Product> getAllProducts() 
+	{
+		
+		return repository.findAll();
+	}
+	public Product getProductById(int id) 
+	{
+		return repository.findById(id).orElse(null);
+	}
+	public Product addProduct(Product product, MultipartFile imageFile) throws IOException 
+	{
+		product.setImageName(imageFile.getOriginalFilename());
+		product.setImageType(imageFile.getContentType());
+		product.setImageData(imageFile.getBytes());
+		return repository.save(product);
+	}
+	public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException 
+	{
+		product.setImageData(imageFile.getBytes());
+		product.setImageName(imageFile.getOriginalFilename());
+		product.setImageType(imageFile.getContentType());
+		return repository.save(product);
+	}
+	public void deleteProduct(int id) 
+	{
+		repository.deleteById(id);
+	}
+	public List<Product> searchProducts(String keyword)
+	{
+		
+		return repository.searchProducts(keyword);
+	}
+	
+}
